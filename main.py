35c137546f70fc50279894b1d3ed1455d2efea9e
solcanma <gemcatko@gmail.com>
2019-01-20 21:26:35 +0100

    u id este neni hotove

--- main.py
+++ main.py
@@ -16,21 +16,23 @@ y_norm_last = 0
 #TODO how to triger saw https://www.sick.com/es/en/registration-sensors/luminescence-sensors/lut9/lut9b-11626/p/p143229  (light? maybe) SEMI TRANSPARENT GLASS WITH WARM WHITE LED OR red light
 #TODO Solve how to triger sensor from code? => https://learn.adafruit.com/adafruit-ft232h-breakout/linux-setup check if possible with python 3
 #TODO give objecs uniqe ID
-#TODO calculate sped of objects
+#TODO calculate speed of objects
 #TODO Show found erros with ID on separate screen
 
 
 def chyba_one_mark_small(cat, score, x, y, w, h, ):
     catdecoded = cat.decode("utf-8")
     x_norm, y_norm, w_norm, h_norm,volume_norm = calculate_volume_norm(x, y, w, h)
+
     #W_norm is used_for triger how big error to detect
     if catdecoded == "cell phone" and w_norm <= 1:
         #left line from camera view at object green
         cv2.line(frame, (int(x-w/2),int(y-h/2)),(int(x-w/2),int(y+h/2)),(0,128,0),10,10)
         #right line_from camera view at object orange
         cv2.line(frame, (int(x+w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(0,128,255),10,10)
-        is_near_by_object(cat, score, x, y, w, h)
+        #is_near_by_object(cat, score, x, y, w, h)
         get_uniqe_id2(cat,score, x, y, w, h)
+        #cell_phone[(len(cell_phone)-2)].draw_id()
         #test na zaciatok vychadzania detekovane objektu zo zaberu. Test v 90% obrazovej plochy
         if (x_norm + w_norm/2) > 0.90:
             print("sprav znacku zaciatok koniec ")
@@ -47,11 +49,10 @@ def is_near_by_object (cat, score, x, y, w, h):
     y_norm = y / Yresolution
 
     if (abs(x_norm - x_norm_last ) <= 0.05) and (abs(y_norm - y_norm_last) <= 0.05):
-        print("Je to ten isty object:dif:", abs(x_norm - x_norm_last), abs(y_norm - y_norm_last))
+        print("Je to ten isty object:   dif:", abs(x_norm - x_norm_last), abs(y_norm - y_norm_last))
 
     if (abs(x_norm - x_norm_last ) > 0.05) or (abs(y_norm - y_norm_last) > 0.05):
         print("nie je  ten isty objekt  ", abs(x_norm - x_norm_last), abs(y_norm - y_norm_last))
-        get_uniqe_id2(cat, score, x, y, w, h)
 
     x_norm_last = x_norm
     y_norm_last = y_norm
@@ -59,13 +60,34 @@ def is_near_by_object (cat, score, x, y, w, h):
 
 def get_uniqe_id2(cat , score, x , y , w , h):
     cell_phone.append(Object(cat,score, x, y, w, h))
-    #print(cell_phone[(len(cell_phone)-1)].x_norm)
+    print("len(cell_phone)",len(cell_phone))
+    #if (cell_phone[(len(cell_phone)-1)].is_near_by_object()) == True:
+        #delete last created object in cell_phone[]
+     #   del cell_phone [-1]
+    #  Object.id = Object.id -1
+    for_cyklus()
+
+    #if (cell_phone[(len(cell_phone)-1)].is_near_by_object()) == True:
+     #   del cell_phone [-1]
+      #  Object.id = Object.id -1
+
     pass
+def for_cyklus():
+    for i in range(len(cell_phone)):
+        if (cell_phone[i].is_near_by_object()) == True:
+            #delete last created object in cell_phone[]
+            del cell_phone [-1]
+            Object.id = Object.id -1
+        break
+    pass
+
+
+
+
 
 def calculate_velocity (id, x, y, w, h ):
     #
     pass
-
 
 def calculate_volume_norm(x, y, w, h):
     x_norm = x / Xresolution
@@ -75,23 +97,10 @@ def calculate_volume_norm(x, y, w, h):
     volume_norm = w_norm * h_norm
     return x_norm, y_norm, w_norm, h_norm,volume_norm
 
-class Employee:
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-
-emp_1 = Employee('Marek','Solcanky', 60000 )
-emp_2 = Employee('Gem','Sol', 30000 )
-
-print(emp_1.email)
-print(emp_2.email)
-print(emp_1.fullname())
-
 class Object:
+    id = 0
+    x_norm_last = 0
+    y_norm_last = 0
     def __init__(self,cat,score, x, y, w, h):
         self.cat = cat
         self.score = score
@@ -101,12 +110,26 @@ class Object:
         self.h = h
         self.x_norm = x / Xresolution
         self.y_norm = y / Yresolution
+        Object.id += 1
+        self.id = Object.id
     def smakes(self):
         return '{} {}'.format(self.x, "smakes")
-    def update_x_y(self):
-        if (abs(self.x_norm - self.x_norm_last ) <= 0.05) and (abs(self.y_norm - self.y_norm_last) <= 0.05):
-            print("Je to ten isty object:dif:", abs(self.x_norm - self.x_norm_last), abs(self.y_norm - self.y_norm_last))
-        pass
+    def is_near_by_object(self):
+        if (abs(self.x_norm - Object.x_norm_last ) <= 0.05) and (abs(self.y_norm - Object.y_norm_last) <= 0.05):
+            print("2Je to ten isty object:dif:", abs(self.x_norm - Object.x_norm_last), abs(self.y_norm - Object.y_norm_last), " ako id:", self.id)
+            Object.x_norm_last = self.x_norm
+            Object.y_norm_last = self.y_norm
+            return True
+        else:
+            print("Nie je to ten isty object:dif:", abs(self.x_norm - Object.x_norm_last), abs(self.y_norm - Object.y_norm_last), " ako id:", self.id )
+            Object.x_norm_last = self.x_norm
+            Object.y_norm_last = self.y_norm
+            return False
+    def draw_id(self):
+        #print(self.Object.id)
+         cv2.putText(frame, str(self.id), (int(self.x), int(self.y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 2, 0))
+
+cell_phone.append(Object(cell_phone,0, 0, 0, 0, 0))
 
 if __name__ == "__main__":
     # Optional statement to configure preferred GPU. Available only in GPU version.
@@ -128,7 +151,7 @@ if __name__ == "__main__":
             del dark_frame
             end_time = time.time()
             #print("Elapsed Time:",end_time-start_time)
-            print(results)
+            #print(results)
             #print(results.cat.count("b'cell phone"))
 
             for cat, score, bounds in results:
