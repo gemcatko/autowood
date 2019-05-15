3f660f46250f94dd475e9a674c5783d62a915729
gemcatko <gemcatko@gmail.com>
2019-05-15 21:55:23 +0200

    ramdisk

--- experiment.py
+++ experiment.py
@@ -1,3 +1,6 @@
+#####Prerequzities
+#sudo mount -t tmpfs -o rw,size=500M tmpfs /mnt/ramdisk
+
 from dev_env_vars import *
 from pydarknet import Detector, Image
 import cv2
@@ -5,6 +8,9 @@ from dev_env_vars import *
 import time
 import multiprocessing
 from multiprocessing import Process, Value, Queue
+
+import logging
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s', )
 
 def show_fps(start_time, end_time):
     duration_of_loop = end_time - start_time
@@ -14,17 +20,18 @@ def show_fps(start_time, end_time):
     return FPS
 
 
-def darknet(yolov3_cfg, yolov_weights, cat_encoding, obj_data, q_picture):
+def darknet(yolov3_cfg, yolov_weights, cat_encoding, obj_data, ):
 
     net = Detector(bytes(yolov3_cfg, encoding=cat_encoding), bytes(yolov_weights, encoding=cat_encoding), 0,
                    bytes(obj_data, encoding=cat_encoding), )
     while True:
-        try:
-            dark_frame = q_picture.get()
-            results = net.detect(dark_frame, thresh=detection_treshold)
 
-        except:
-            print("exeption in darknet ocured")
+        read_frame = cv2.imread("/mnt/ramdisk/frame.jpg")
+        dark_frame = Image(read_frame)
+        results = net.detect(dark_frame, thresh=detection_treshold)
+        print("Results:", results)
+        del dark_frame
+
 
 
 
@@ -44,11 +51,9 @@ if __name__ == "__main__":
     #net = Detector(bytes(yolov3_cfg, encoding=cat_encoding), bytes(yolov_weights, encoding=cat_encoding), 0, bytes(obj_data, encoding=cat_encoding), )
 
     q_picture = multiprocessing.Queue()
-    p = Process(target=darknet, args=(yolov3_cfg, yolov_weights, cat_encoding, obj_data,q_picture,))
+    q_results = multiprocessing.Queue()
+    p = Process(target=darknet, args=(yolov3_cfg, yolov_weights, cat_encoding, obj_data,))
     p.start()
-
-
-
     ########################## MAIN LOOP ###############################################################################
 
     while True:
@@ -57,15 +62,27 @@ if __name__ == "__main__":
         if r:
             # start_time = time.time()
             # Only measure the time taken by YOLO and API Call overhead
-            dark_frame = Image(frame)
-            q_picture.put(dark_frame)
+            cv2.imwrite("/mnt/ramdisk/frame.jpg", frame)
+            ##read_frame = cv2.imread("/mnt/ramdisk/frame.jpg")
+            ##dark_frame = Image(read_frame)
             # This are the function parameters of detect:
             # Possible inputs: def detect(self, Image image, float thresh=.5, float hier_thresh=.5, float nms=.45):
             # call Yolo34
-            #results = net.detect(dark_frame, thresh=detection_treshold)
-            del dark_frame
+            ##results = net.detect(dark_frame, thresh=detection_treshold)
+            ##print("Results:",results)
+            """
+
+            try:
+                print("q_results:",q_results.get_nowait())
+            except:
+                time.sleep(0.01)
+                print("exeption in main tread ecored")
+
+            ##del dark_frame
+            """
         #cv2.imshow("preview", frame)
         # print("Elapsed Time:",end_time-start_time)
+
         k = cv2.waitKey(1)
         end_time=time.time()
         show_fps(start_time,end_time)
