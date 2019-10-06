f3f3b7e7e98326c453ef401c607d16f53d955f2d
gemcatko <gemcatko@gmail.com>
2019-10-06 21:00:37 +0200

    added brown color, trail visualization is working

--- draw_trail_visualization.py
+++ draw_trail_visualization.py
@@ -15,23 +15,28 @@ def convert_from_xywh_to_xAyAxByB_format(bounds):
     return box
 
 def dpi_to_pixels(dpi):
-     return (Xresolution / size_of_one_screen_in_dpi) * dpi
+    """
+
+    :param dpi: angle in dpi which you would like to convert to pixels . It is using global variable size_of_one_screen_in_dpi which is defined in dev_env_wars
+    :return: pixels
+    """
+    return (Xresolution / size_of_one_screen_in_dpi) * dpi
 
 def draw_trail_visualization(objeky,s_distance):
     trail_visualization = np.zeros((int(Yresolution / scale_trail_visualization), Xresolution * 2, 3),
                                    dtype="uint8")
     for id in objekty:
         xA, yA, xB, yB = convert_from_xywh_to_xAyAxByB_format(objekty[id].bounds)
-        if objekty[id].is_detected_by_detector == False:
-            visualization_xA = xA + dpi_to_pixels(objekty[id].position_on_trail) - dpi_to_pixels(s_distance.value)
-            visualization_xB = xB + dpi_to_pixels(objekty[id].position_on_trail) - dpi_to_pixels(s_distance.value)
-            if objekty[id].category == "error" or objekty[id].category == "eye" or  objekty[id].category == "crack":
-                cv2.rectangle(trail_visualization, (int(visualization_xA), int(yA / scale_trail_visualization)),
-                              (int(visualization_xB), int(yB / scale_trail_visualization)), red)
-            #else:
-            #    cv2.rectangle(trail_visualization, (int(visualization_xA), int(yA / scale_trail_visualization)),
-            #                  (int(visualization_xB), int(yB / scale_trail_visualization)), green)
-
-        "#TODO draw objects if not detected anymore"
-
+        #if objekty[id].is_detected_by_detector == False or True:
+        #calcculate begining xA and endig xB of rectangle in trai_visualization
+        visualization_xA = xA + dpi_to_pixels(objekty[id].position_on_trail) - dpi_to_pixels(s_distance.value)
+        visualization_xB = xB + dpi_to_pixels(objekty[id].position_on_trail) - dpi_to_pixels(s_distance.value)
+        #draw error, eye, crack, rot, and crust as red color
+        if objekty[id].category == "error" or objekty[id].category == "eye" or objekty[id].category == "crack" or objekty[id].category == "rot" or objekty[id].category == "crust":
+            cv2.rectangle(trail_visualization, (int(visualization_xA), int(yA / scale_trail_visualization)),
+                          (int(visualization_xB), int(yB / scale_trail_visualization)), red, 3)
+        #draw secondclass as brown collor
+        elif objekty[id].category == "secondclass" or objekty[id].category == "zapar" or objekty[id].category == "darksecondclass" :
+            cv2.rectangle(trail_visualization, (int(visualization_xA), int(yA / scale_trail_visualization)),
+                          (int(visualization_xB), int(yB / scale_trail_visualization)), brown, 2)
     cv2.imshow("Trail_visualization", trail_visualization)
